5dca7f8d79ab2fb4cd3129773dc225e7b5955f92
silicate harvester <32470673-silicateharvester@users.noreply.gitlab.com>
2025-12-20 17:14:54 +0100

    Rename spellEmbed to genericEmbed + remove specific functionality

---
+++ embeds/genericEmbed.js
@@ -0,0 +1,13 @@
+const { EmbedBuilder } = require('discord.js');
+
+module.exports = {
+	/* todo: add build for content (hopefully) */
+	genericEmbed: async (title) => {
+
+		return new EmbedBuilder()
+			.setTitle(title)
+			.addFields(
+				{ name: 'test', value: 'test' },
+			);
+	},
+};
