8d80ddc4949a00e084953d28f27fd0b455b9b3bf
silicate harvester <32470673-silicateharvester@users.noreply.gitlab.com>
2025-12-20 11:17:54 +0100

    Add first draft of fetching and showing page info - BROKEN DO NOT USE

--- embeds/spellEmbed.js
+++ embeds/spellEmbed.js
@@ -1,15 +1,17 @@
 const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
-	spellEmbed: async (title, description, classes, source) => {
+	spellEmbed: async (title, description) => {
 		return new EmbedBuilder()
 			.setColor(0x524b9e)
 			.setTitle(title)
 			.addFields(
+				/*
 				{ name: 'Title', value: title },
 				{ name: '\u200B', value: '\u200B' },
 				{ name: 'Classes', value: classes, inline: true },
 				{ name: 'Source', value: source, inline: true },
+				*/
 				{ name: 'Description', value: description, inline: true },
 			);
 	},
