250fbff56ddd031a793e581079f75bfbb3e7581d
silicate harvester <32470673-silicateharvester@users.noreply.gitlab.com>
2025-12-20 11:18:12 +0100

    Refactor main.js to use CommonJS instead of ES6 module syntax

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-import { Client, Events, GatewayIntentBits } from 'discord.js';
-import 'dotenv/config';
+const path = require('node:path');
+const fs = require('node:fs');
+const { Client, Collection, Events, GatewayIntentBits, MessageFlags } = require('discord.js');
+require('dotenv').config;
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
@@ -8,3 +10,60 @@ client.once(Events.ClientReady, (readyClient) => {
 });
 
 client.login(process.env.BOT_TOKEN);
+
+client.commands = new Collection();
+
+// ------------------------
+
+const folderName = 'src/commands';
+const foldersPath = path.join(__dirname, folderName);
+
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith('.js'));
+
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		}
+		else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		};
+	};
+};
+
+// -------------------------
+
+client.on(Events.InteractionCreate, async (interaction) => {
+	if (!interaction.isChatInputCommand()) return;
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found`);
+		return;
+	};
+
+	try {
+		await command.execute(interaction);
+	}
+	catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({
+				content: 'There was an error while executing this command!',
+				flags: MessageFlags.Ephemeral,
+			});
+		}
+		else {
+			await interaction.reply({
+				content: 'There was an error while executing this command!',
+				flags: MessageFlags.Ephemeral,
+			});
+		}
+	};
+});
