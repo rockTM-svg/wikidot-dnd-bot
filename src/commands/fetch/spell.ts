1e4843d7ce1b45bff8f78735d8b16570f0f3aa8b
silicate harvester <32470673-silicateharvester@users.noreply.gitlab.com>
2025-12-28 21:28:50 +0100

    Refactor spell.ts to Typescript with new interfaces

---
+++ src/commands/fetch/spell.ts
@@ -0,0 +1,58 @@
+import { ChatInputCommandInteraction, SlashCommandBuilder } from 'discord.js';
+// const { genericEmbed } = require('../../embeds/genericEmbed.js');
+import parseHTML from '../../utility/parseHTML.js';
+import ParsedHTMLText from '../../interface/parsedHTMLText.js';
+
+import sites from '../../../sites.json' with {'type': 'json'};
+
+// ----------------------------------
+
+export const data = new SlashCommandBuilder()
+	.setName('spell')
+	.setDescription('Returns information about spell if it exists')
+	.addStringOption((option) =>
+		option
+			.setName('system')
+			.setDescription('System from where the spell is from')
+			.setRequired(true)
+			.addChoices(
+				{ name: 'DND 5e', value: 'dnd5e' },
+				{ name: 'DND 5e (2024)', value: 'dnd5e2024' },
+			))
+	.addStringOption((option) =>
+		option
+			.setName('name')
+			.setDescription('Spell name')
+			.setRequired(true));
+
+export const execute = async (interaction: ChatInputCommandInteraction) => {
+	await interaction.deferReply();
+
+	const option = interaction.options.getString('system');
+	const spellName = interaction.options.getString('name');
+
+	let url = '';
+	switch (option) {
+	case 'dnd5e':
+		url = `${sites.dnd5e}/spell:${spellName}`;
+		break;
+	case 'dnd2024':
+		url = `${sites.dnd2024}/spell:${spellName}`;
+		break;
+	};
+
+	// todo: check against 404s
+	const tempcontent = await fetch(url)
+		.then((res) => res.text())
+		.then(text => parseHTML(text));
+
+	let content = '';
+	tempcontent.forEach((value: ParsedHTMLText) => {
+		content += value.content + '\n\n';
+	});
+
+	await interaction.editReply(
+		`\n**${option} - ${spellName}**\n\n` +
+		`${content}`,
+	);
+};
