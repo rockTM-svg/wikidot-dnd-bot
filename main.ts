8990513036508b10bd9f8324d3077f5d083b5a51
silicate harvester <32470673-silicateharvester@users.noreply.gitlab.com>
2026-01-05 19:15:58 +0100

    Replace dotenv with dotenvx - Preparing for proper Docker release

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 import path from "node:path";
 import fs from "node:fs";
-import dotenv from "dotenv";
+import dotenvx from "@dotenvx/dotenvx";
 
 import { fileURLToPath } from "node:url";
 import {
@@ -16,7 +16,7 @@ import type DiscordChatCommand from "root/interface/discordCommand.js";
 
 // -------------------------
 
-dotenv.config();
+dotenvx.config();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
