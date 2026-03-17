ff2dbe24019152acaff0bed36b774c432dc6d942
rockTM-svg <195896211+rockTM-svg@users.noreply.github.com>
2026-03-17 18:39:00 +0100

    Remove leftover type file

--- src/@types/discord.d.ts
+++
@@ -1,9 +0,0 @@
-import type { Collection } from "discord.js";
-
-import type DiscordChatCommand from "../interface/discordCommand.ts";
-
-declare module "discord.js" {
-	export interface Client {
-		commands: Collection<string, DiscordChatCommand>;
-	}
-}
